f69c6815bd8f05a56d65380be6abf9c34e5e263f
jiwon79 <19-079@ksa.hs.kr>
2021-12-18 23:32:12 +0900

    feat: Add item add button

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,55 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo List',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Todo List')
-        ),
-      )
+    return MaterialApp(title: 'Todo List', home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final List<String> _todoItems = [];
+
+  void _addtodoItem() {
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item' + index.toString());
+    });
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemCount: _todoItems.length,
+      itemBuilder: (context, index) {
+        print(index);
+        return _buildTodoItem(_todoItems[index]);
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText) {
+    return ListTile(
+      title: Text(todoText),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addtodoItem,
+        tooltip: 'Add task',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
